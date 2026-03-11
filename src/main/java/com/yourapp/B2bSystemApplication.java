e1bb8098a7544fce3c28054cc36bea1be3a5388b
anton <147056808+antonkol1993@users.noreply.github.com>
2026-03-11 14:05:44 +0000

    Spring Boot project created

---
+++ src/main/java/com/yourapp/B2bSystemApplication.java
@@ -0,0 +1,13 @@
+package com.yourapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2bSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2bSystemApplication.class, args);
+	}
+
+}
